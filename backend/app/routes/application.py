94a399a51905aac1ace8577abdc4f75052f9bf35
Jason Eckardt-Taing <jason.eckardttaing@creekenterprise.com>
2025-11-18 14:04:07 -0500

    update flavor text

--- backend/app/routes/application.py
+++ backend/app/routes/application.py
@@ -6,9 +6,11 @@ application = Blueprint("application", __name__)
 
 @application.route("/")
 def index():
-    return f'Welcome to party jam! <a href="{os.getenv("BACKEND_URL")}/me">Login</a>'
+    return (
+        f'Welcome to the party jam! <a href="{os.getenv("BACKEND_URL")}/me">Login</a>'
+    )
 
 
 @application.route("/status")
 def status():
-    return "Spotify Jam server v0.3.1 running"
+    return "Party Jam server v0.4.2 running"

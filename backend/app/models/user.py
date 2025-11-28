46afe1aa1be9305f7ef6037caddb0f92299fdefa
JasonEckardt <techbase2704@gmail.com>
2025-11-28 10:48:08 -0500

    add user_role attribute

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -6,11 +6,11 @@ class User(db.Model):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.String(128), nullable=False, unique=True)
     access_token = db.Column(db.String(512), nullable=False)
-    refresh_token = db.Column(db.String(512), nullable=False)
     expires_at = db.Column(db.DateTime(timezone=True), nullable=False)
-    # TODO: Add user_role attribute
+    refresh_token = db.Column(db.String(512), nullable=False)
+    user_role = db.Column(db.String(32), nullable=False, default="user")
+    user_id = db.Column(db.String(128), nullable=False, unique=True, index=True)
 
     def is_expired(self):
         if self.expires_at.tzinfo is None:
